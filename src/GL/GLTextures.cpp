8cc3257b01a74db76305ca60f195d167e4d34732
brown-pants <z19986938930@163.com>
2025-11-10 15:21:38 +0800

    NULL to 0

--- src/GL/GLTextures.cpp
+++ src/GL/GLTextures.cpp
@@ -53,7 +53,7 @@ GLDepthMap::GLDepthMap() : GLTexture(Tex2d)
 void GLDepthMap::setData(GLsizei width, GLsizei height)
 {
 	this->bind();
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
